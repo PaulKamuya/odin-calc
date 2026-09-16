48c3484eac626319808ea4b4aa6b21d8131466c2
Paul <147955161+PaulKamuya@users.noreply.github.com>
2026-09-16 17:43:23 +0300

    Added operation functionality

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const btn = document.querySelector('button')
 btn.addEventListener('click', () => {
   display.value += btn.textContent
 })
+
+const add = function (a,b) {
+  console.log(a + b)
+  return a + b
+}
+
+const sub = (a,b) => {
+  console.log(a - b)
+  return a - b
+}
+
+const multiply = (a,b) => {
+  console.log(a * b)
+  return a * b
+}
+
+const divide = (a,b) => {
+  console.log(a / b)
+  return a / b
+}
+
+add(5, 6) // 11
+sub(10, 9) // 1
+multiply(1, 7) // 7
+divide (10, 10) // 1
