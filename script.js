778d97173339209771bc6b8b9471087c822a17fb
Paul <147955161+PaulKamuya@users.noreply.github.com>
2026-09-18 01:47:20 +0300

    Added test for operator buttons

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const display = document.querySelector('#display')
 const btn = document.querySelectorAll('.btn')
 const operatorsBtn = document.querySelectorAll('[id]')
 
+operatorsBtn.forEach(operator => {
+  operator.addEventListener('click', () => {
+    console.log(operator.id)
+  })
+})
